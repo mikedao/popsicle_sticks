afb01ead49b2e9c0b49c8248c575b9b7758f19ee
Michael Dao <mike@turing.edu>
2022-07-27 09:06:39 -0600

    adds create student functionality

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+  def new
+    @mod_id = params[:id]
+  end
+
+  def create
+    Student.create( 
+                  name: params[:name],
+                  pronouns: params[:pronouns],
+                  called_on_count: 0
+                  mod_id: params[:id]
+                )
+    redirect_to mod_path(params[:id])
+  end
+
+
+end
