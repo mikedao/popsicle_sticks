aeb9f1fbf89078167c8c286397d62b17f49f908d
Michael Dao <mike@turing.edu>
2022-07-27 11:31:09 -0600

    adds pick a student

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -6,4 +6,10 @@ class ModsController < ApplicationController
   def show
     @mod = Mod.find(params[:id])
   end
+
+  def lucky
+    mod = Mod.find(params[:id])
+    @student = mod.students.order(Arel.sql('RANDOM()')).first
+    @student.increment!(:called_on_count)
+  end
 end
